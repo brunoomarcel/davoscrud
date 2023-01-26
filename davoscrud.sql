2362c5dc76696c6eef353229e81b28128354a8c6
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2023-01-25 22:06:32 -0300

    commit-04

---
+++ davoscrud.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26-Jan-2023 às 02:04
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `davoscrud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefone` varchar(16) NOT NULL,
+  `situacao` varchar(12) NOT NULL,
+  `mensalidade` varchar(32) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `observacao` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `telefone`, `situacao`, `mensalidade`, `senha`, `observacao`) VALUES
+(9, 'Fernanda Albuquerque', 'fernanda@outlook.com', '79988563259', 'ativa', 'R$89,36', '0321456987br', 0),
+(10, 'julio', 'julio@hotmail.com', '79988563256', 'inativo', '58', '159753654', 0),
+(11, 'Tulio Garcia', 'tulio@outlink.com.br', '79985632145', 'inativo', '123', '03ssdd7br', 0),
+(12, 'Alice Senna', 'alice@hotmail.com', '98998877654', 'ativo', '78,9', 'padraosenha', 0),
+(13, 'gabriel', 'gabriel@uuu.com', '91187896541', 'ativo', '85', '0321456987br', 0),
+(14, 'victor', 'victor@abara.com', '79898863256', 'ativo', '58,3', 'padraosenha', 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
