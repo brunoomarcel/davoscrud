81c000d2a7d2cb5070defaf86af9b9e3cf1ee1f3
Victor Matheus <victormatheus183@gmail.com>
2023-01-30 22:14:46 -0300

    Feedback for registration applied

--- Controller/StudentController.php
+++ Controller/StudentController.php
@@ -6,6 +6,7 @@ class StudentController{
 
     function __construct(){
         $this->html = file_get_contents(PROJECT_ROOT.'views/register.html');
+        $this->html = str_replace("{{feedback}}", '' , $this->html);
     }
 
     public function listStudents(){
@@ -13,8 +14,24 @@ class StudentController{
         $students = 0;
         for ($i=0; $i < 10; $i++) { 
             $students++;
+    public function storeStudent(){
+        if(isset($_REQUEST) && !empty($_REQUEST)){
+            $this->resetHtml('views/register.html');
+            if(Student::store($_REQUEST)){
+                $feedback = $this->getRegistrationFeedback('success', 'Aluno Cadastrado com sucesso!');
+                $this->html = str_replace("{{feedback}}", $feedback , $this->html);
+                
+            }else{
+                $feedback = $this->getRegistrationFeedback('failure', 'Erro ao cadastrar aluno!');
+                $this->html = str_replace("{{feedback}}", $feedback , $this->html);
+            }
         }
         $this->html = str_replace('{{list}}', $students, $this->html);
+    }
+    private function getRegistrationFeedback($component = 'success', $msg = 'Student registered'){
+        $feedbackComponent = file_get_contents(PROJECT_ROOT.'views/components/feedback/'.$component.'.html');
+        $feedbackComponent = str_replace('{{msg}}', $msg, $feedbackComponent);
+        return $feedbackComponent;
     }
 
     public function storeStudent(){
