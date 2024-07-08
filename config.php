4d49b7ed2d397de8c7995a5cee6d6249157e8c4d
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2024-07-08 12:56:29 -0300

    fix: update constant DOCUMENT_ROOT to dirname(__DIR__)

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-define('PROJECT_ROOT', $_SERVER["DOCUMENT_ROOT"].'/davoscrud/');
-
+define('PROJECT_ROOT', dirname(__DIR__) . '/davoscrud/');
+// antes era define('PROJECT_ROOT', $_SERVER["DOCUMENT_ROOT"].'/davoscrud/');
